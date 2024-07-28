cf9db3462b6ba19263231c3d6416584c21fd65b8
Li[李] <aliimrandtb@gmail.com>
2024-07-28 16:38:59 +0700

    [ADD] alnas_docx: render_image function

--- alnas_docx/models/ir_actions_report.py
+++ alnas_docx/models/ir_actions_report.py
@@ -1,12 +1,15 @@
 import base64
-from io import BytesIO
 import zipfile
-from docxtpl import DocxTemplate
+from io import BytesIO
+from functools import partial
+from docxtpl import DocxTemplate, InlineImage
 from docxcompose.composer import Composer
 from docx import Document
+from docx.shared import Mm
 from bs4 import BeautifulSoup
 from num2words import num2words
 from babel.dates import format_date
+from babel.numbers import format_decimal
 from htmldocx import HtmlToDocx
 
 from odoo import _, api, fields, models
@@ -14,76 +17,84 @@ from odoo.tools.safe_eval import safe_eval, time
 from odoo.exceptions import ValidationError
 
 
-
 class IrActionsReport(models.Model):
     _inherit = "ir.actions.report"
 
-    report_type = fields.Selection( selection_add=[("docx", "DOCX")], ondelete={"docx": "cascade"})
-    report_docx_template = fields.Binary(string="Report DOCX Template")    
+    report_type = fields.Selection(
+        selection_add=[("docx", "DOCX")], ondelete={"docx": "cascade"}
+    ) # add docx type
+    report_docx_template = fields.Binary(string="Report DOCX Template")
     report_docx_template_name = fields.Char(string="Report DOCX Template Name")
-    docx_merge_mode = fields.Selection([("composer", "Composer"), ("zip", "Zip")],
+    docx_merge_mode = fields.Selection(
+        [("composer", "Composer"), ("zip", "Zip")],
         string="DOCX Mode",
-        default="composer"
+        default="composer",
     )
-    
-    @api.constrains('report_type')
+
+    @api.constrains("report_type")
     def _check_report_type(self):
         for rec in self:
-            if rec.report_type == 'docx' and not rec.report_docx_template and not rec.report_docx_template_name.endswith('.docx'):
+            if (
+                rec.report_type == "docx"
+                and not rec.report_docx_template
+                and not rec.report_docx_template_name.endswith(".docx")
+            ):
                 raise ValidationError(_("Please upload a DOCX template."))
-            
+
     def _render_docx(self, report_ref, docids, data):
         report = self._get_report_from_name(report_ref)
         template = report.report_docx_template
-        
+
         if not template:
             raise ValueError("No DOCX template found.")
-        
+
         doc_template = DocxTemplate(BytesIO(base64.b64decode(template)))
         doc_obj = self.env[report.model].browse(docids)
-        
+        render_image = partial(self._render_image, doc_template)
+        html2docx = partial(self._render_html_as_subdoc, doc_template)
+
         context = {
-            'spelled_out': self._spelled_out,
-            'parsehtml': self._parse_html,
-            'formatdate': self._formatdate,
-            'company': self.env.company,
-            'lang' : self._context.get('lang', 'id_ID'),
-            'sysdate': fields.Datetime.now(),
+            "spelled_out": self._spelled_out,
+            "parsehtml": self._parse_html,
+            "formatdate": self._formatdate,
+            "company": self.env.company,
+            "lang": self._context.get("lang", "id_ID"),
+            "sysdate": fields.Datetime.now(),
+            "render_image": render_image,
+            "html2docx": html2docx,
+            "convert_currency": self._convert_currency,
         }
-        
-        if report.docx_merge_mode == 'composer':
+
+        if report.docx_merge_mode == "composer":
             return self._render_composer_mode(doc_template, doc_obj, data, context)
-        elif report.docx_merge_mode == 'zip':
-            return self._render_zip_mode(doc_template, doc_obj, data, context, report_name=report.print_report_name)
+        elif report.docx_merge_mode == "zip":
+            return self._render_zip_mode(
+                doc_template,
+                doc_obj,
+                data,
+                context,
+                report_name=report.print_report_name,
+            )
         else:
             return self._render_docx_to_pdf_mode(doc_template, doc_obj, data, context)
-        
+
     def _render_composer_mode(self, doc_template, doc_obj, data, context):
         master_doc = Document()
         composer = Composer(master_doc)
-        
+
         for idx, obj in enumerate(doc_obj):
             context = {
                 **context,
-                'docs': obj,
-                'data': data,
-                'html' : {},
+                "docs": obj,
+                "data": data,
+                "html": {},
             }
-            
-            for field in obj._fields:
-                value = getattr(obj, field)
-                if isinstance(value, str) and self.is_html(value):
-                    subdoc_stream  = self.render_html_as_subdoc(value)
-                    sub_doc = doc_template.new_subdoc(subdoc_stream)
-                    context['html'][field] = sub_doc
-                else:
-                    context['html'][field] = ''
-                
+
             temp = BytesIO()
             doc_template.render(context)
             doc_template.save(temp)
             temp.seek(0)
-            
+
             if len(doc_obj) == 1:
                 return temp.read()
             else:
@@ -101,34 +112,27 @@ class IrActionsReport(models.Model):
 
         return temp_output.read()
 
-    def _render_zip_mode(self, doc_template, doc_obj, data, context, report_name='report'):
+    def _render_zip_mode(
+        self, doc_template, doc_obj, data, context, report_name="report"
+    ):
         docx_files = []
 
         for obj in doc_obj:
             context = {
                 **context,
-                'docs': obj,
-                'data': data,
-                'html' : {},
+                "docs": obj,
+                "data": data,
+                "html": {},
             }
-            
-            for field in obj._fields:
-                value = getattr(obj, field)
-                if isinstance(value, str) and self.is_html(value):
-                    subdoc_stream  = self.render_html_as_subdoc(value)
-                    sub_doc = doc_template.new_subdoc(subdoc_stream)
-                    context['html'][field] = sub_doc
-                else:
-                    context['html'][field] = ''
-            
+
             temp = BytesIO()
             doc_template.render(context)
             doc_template.save(temp)
             temp.seek(0)
             docx_files.append(temp.read())
-            
+
         zip_buffer = BytesIO()
-        with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+        with zipfile.ZipFile(zip_buffer, "w") as zip_file:
             for idx, docx_file in enumerate(docx_files):
                 name = safe_eval(report_name, {"object": doc_obj[idx], "time": time})
                 filename = "%s.%s" % (name, "docx")
@@ -137,38 +141,58 @@ class IrActionsReport(models.Model):
         zip_buffer.seek(0)
 
         return zip_buffer.read()
-    
+
     # Next Improvement
     def _render_docx_to_pdf_mode(self, doc_template, doc_obj, data, context):
         raise NotImplementedError("This method is not implemented yet.")
-    
+
     def _convert_docx_to_pdf(self, docx_stream):
         raise NotImplementedError("This method is not implemented yet.")
-    
-    # Render Function
+
+    @staticmethod
+    def _render_image(tpl, imgb64, width=None, height=None):
+        width = Mm(width) if width else None
+        height = Mm(height) if height else None
+        
+        if not imgb64:
+            return ''
+
+        image_stream = BytesIO(base64.b64decode(imgb64))
+        return InlineImage(
+            tpl, image_descriptor=image_stream, width=width, height=height
+        )
+
     @staticmethod
     def _parse_html(html):
         if not html:
-            return ''
-        soup = BeautifulSoup(html, 'html.parser')
+            return ""
+        soup = BeautifulSoup(html, "html.parser")
         return soup.get_text()
-    
+
     @staticmethod
-    def _formatdate(date_required=fields.Datetime.today(), format='full', lang='id_ID'):
+    def _formatdate(date_required=fields.Datetime.today(), format="full", lang="id_ID"):
         return format_date(date_required, format=format, locale=lang)
+
+    @staticmethod
+    def _spelled_out(number, lang="id_ID", to="cardinal"):
+        return num2words(number, lang=lang, to=to)
     
     @staticmethod
-    def _spelled_out(number, lang='id_ID'):
-        return num2words(number, lang=lang)
-    
-    def render_html_as_subdoc(self, html_code):
+    def _convert_currency(number, currency_field, locale='id_ID'):
+        return format_decimal(number, currency_field.name, locale)
+
+    @staticmethod
+    def _render_html_as_subdoc(tpl, html_code=None):
+        if not (
+            isinstance(html_code, str)
+            and bool(BeautifulSoup(html_code, "html.parser").find())
+        ):
+            return ""
+
         temp = BytesIO()
         desc_document = Document()
         new_parser = HtmlToDocx()
         new_parser.add_html_to_document(html_code, desc_document)
         desc_document.save(temp)
         temp.seek(0)
-        return temp
-    
-    def is_html(self, text):
-        return bool(BeautifulSoup(text, "html.parser").find())
+        return tpl.new_subdoc(temp)

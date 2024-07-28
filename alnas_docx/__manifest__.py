7a953dc3709099ffb51efa7ed7fb61c443f57e3e
Li[李] <aliimrandtb@gmail.com>
2024-07-28 17:02:17 +0700

    [REM] alnas_docx: babel and num2word already exist in odoo requirements

--- alnas_docx/__manifest__.py
+++ alnas_docx/__manifest__.py
@@ -41,7 +41,7 @@
     'license': 'LGPL-3',
     
     'external_dependencies': {
-        'python': ['docxtpl', 'docxcompose', 'htmldocx', 'num2words', 'Babel', 'beautifulsoup4'],
+        'python': ['docxtpl', 'docxcompose', 'htmldocx', 'beautifulsoup4'],
     }
     
 }

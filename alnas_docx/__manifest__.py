cd4755c23eb086f26bf89f398092e53f422ba956
kowalski <pksgmaster@gmail.com>
2025-11-17 07:55:45 +0700

    [FIX] alnas_docx: change library docxcompose

--- alnas_docx/__manifest__.py
+++ alnas_docx/__manifest__.py
@@ -45,7 +45,7 @@
     'license': 'LGPL-3',
     
     'external_dependencies': {
-        'python': ['docxtpl', 'htmldocx'],
+        'python': ['docxtpl', 'docxcompose', 'htmldocx'],
     }
     
 }
